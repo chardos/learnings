7507e63b48188db26a9f6d21d7a98679a6b3009f
richard tan <richard.tan@crownbet.com.au>
2016-10-19 22:12:22 +1100

    more generator examples

--- javascript/generators/generators.js
+++ javascript/generators/generators.js
@@ -13,6 +13,43 @@ function* sequence(){
 
 var seq = sequence();
 
-console.log(gen.next().value); // Drop in
-console.log(gen.next().value); // Kickflip
-console.log(gen.next().value); // Backflip
+console.log(seq.next().value); // Drop in
+console.log(seq.next().value); // Kickflip
+console.log(seq.next().value); // Backflip
+
+// =============================================================================
+// Example with yield*
+// =============================================================================
+
+function* likeGenerator(str){
+    yield `I like ${str}`
+    yield* dislikeGenerator(str)
+    yield `I love ${str}`
+}
+function* dislikeGenerator(str){
+    yield `I dislike ${str}`
+    yield `I loathe ${str}`
+}
+let seq = likeGenerator('spoons')
+
+console.log(seq.next().value); // I like spoons
+console.log(seq.next().value); // I dislike spoons
+console.log(seq.next().value); // I loathe spoons
+console.log(seq.next().value); // I love spoons
+
+
+// =============================================================================
+// Passing arguments into generators
+// =============================================================================
+
+function* generator(){
+    console.log(yield);
+    alert(yield);
+}
+var gen = generator()
+
+// the first call of next executes from the start of the function
+// until the first yield statement
+gen.next(); 
+gen.next('cats'); // logs
+gen.next('dogs'); // alerts
