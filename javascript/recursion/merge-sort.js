9699829fc114ac0b5ef5610fd4b3be1e0b2793de
Richard Tan <richard.tan@crownbet.com.au>
2016-12-19 23:04:34 +1100

    Merge sort... but how ?!

--- javascript/recursion/merge-sort.js
+++ javascript/recursion/merge-sort.js
@@ -18,11 +18,22 @@ const merge = (left, right, sorted = []) => {
     }
 }
 
-merge([2,3,5,12,27], [2,9,11,23,42,]);
+// merge([2,3,5,12,27], [2,9,11,23,42]);
 
+function mergeSort(items){
+    debugger;
+    // Terminal case: 0 or 1 item arrays don't need sorting
+    if (items.length < 2) {
+        return items;
+    }
 
-const mergeSort = (array) => {
-    // break array into singles list
+    var middle = Math.floor(items.length / 2),
+        left    = items.slice(0, middle),
+        right   = items.slice(middle);
 
-    const mid = Math.floor()
+    //Still NFI how the below statement works
+    return merge(mergeSort(left), mergeSort(right));
 }
+
+// mergeSort([7,3,43,87,43,13,75,3,74,9,99])
+mergeSort([7,3,4,1])
