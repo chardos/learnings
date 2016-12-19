a28c5a7b81040babcfb6e3d1ad6e3d4c94cdfa98
Richard Tan <richard.tan@crownbet.com.au>
2016-12-20 00:19:22 +1100

    Merge sort sorted

--- javascript/recursion/merge-sort.js
+++ javascript/recursion/merge-sort.js
@@ -3,8 +3,8 @@ const merge = (left, right, sorted = []) => {
 
     if(!left[0] && !right[0]) return sorted;
 
-    const takeLeft = left[0] < right[0] ||
-                     !right[0] ||
+    const takeLeft = !right[0] ||
+                     left[0] < right[0] ||
                      left[0] === right[0]
 
     const takeRight = right[0] < left[0] ||
@@ -17,9 +17,30 @@ const merge = (left, right, sorted = []) => {
         return merge( left, right.slice(1), [...sorted, right[0]] )
     }
 }
-
 // merge([2,3,5,12,27], [2,9,11,23,42]);
 
+// My merge sort
+const mergeSort = (array, mergedArray = [], originalLength = array.length) => {
+    debugger;
+    if (mergedArray[0] && mergedArray[0].length === originalLength) return mergedArray[0];
+    if (array.length == 0){
+        return mergeSort( mergedArray, [], originalLength )
+    }
+    else {
+        // First pass, wrap the numbers in arrays
+        if (typeof array[0] == 'number') array[0] = [array[0]];
+        if (typeof array[1] == 'number') array[1] = [array[1]];
+        return mergeSort(
+            array.slice(2),
+            [ ...mergedArray, merge(array[0], array[1] || []) ],
+            originalLength
+        )
+    }
+}
+mergeSort([7, 3, 4, 1, 5])
+
+
+// Merge sort from the web
 function mergeSort(items){
     debugger;
     // Terminal case: 0 or 1 item arrays don't need sorting
