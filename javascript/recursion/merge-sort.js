ef7fe48fe9eb7b835a6e81e8b6489cd194e47d22
Richard Tan <richard.tan@crownbet.com.au>
2016-12-19 22:22:48 +1100

    merge function done

--- javascript/recursion/merge-sort.js
+++ javascript/recursion/merge-sort.js
@@ -1,3 +1,28 @@
-const mergeSort = (array) => {
+// Takes two already sorted arrays, and merge them together
+const merge = (left, right, sorted = []) => {
 
+    if(!left[0] && !right[0]) return sorted;
+
+    const takeLeft = left[0] < right[0] ||
+                     !right[0] ||
+                     left[0] === right[0]
+
+    const takeRight = right[0] < left[0] ||
+                      !left[0]
+
+    if(takeLeft){
+        return merge( left.slice(1), right, [...sorted, left[0]] )
+    }
+    else if (takeRight){
+        return merge( left, right.slice(1), [...sorted, right[0]] )
+    }
+}
+
+merge([2,3,5,12,27], [2,9,11,23,42,]);
+
+
+const mergeSort = (array) => {
+    // break array into singles list
+
+    const mid = Math.floor()
 }
