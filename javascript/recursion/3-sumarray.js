51fb0ff02a89fd739d9beb6127b3581ae25cd828
Richard Tan <richard.tan@crownbet.com.au>
2016-12-19 20:50:07 +1100

    Recursion: sumarray

--- javascript/recursion/3-sumarray.js
+++ javascript/recursion/3-sumarray.js
@@ -1,3 +1,7 @@
-let sumArray = () => {
-    
+const sumArray = (numArray, accumulator = 0) => {
+    if (numArray.length === 0) return accumulator;
+    const [head, ...tail] = numArray;
+    return sumArray(tail, accumulator + head);
 }
+
+console.log( sumArray([3,9,5,3]) );
