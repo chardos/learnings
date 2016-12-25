c1eb5b4024bb6bfcce3fcd45dfb32f6f13779637
Richard Tan <richard.tan@crownbet.com.au>
2016-12-25 18:16:35 +1100

    Check object refactored

--- javascript/recursion/check-object.js
+++ javascript/recursion/check-object.js
@@ -1,34 +1,21 @@
 function checker(currentObject, toCheck){
-    console.log('===================');
-    console.log('currentObject',currentObject);
-    console.log('toCheck',toCheck);
-    console.log('length', toCheck.length);
+    const noObjectsLeft = toCheck.length === 0;
+    const objectExists = currentObject && noObjectsLeft;
 
-
-    console.log('bool', !!currentObject);
-    if(currentObject){
-        if (toCheck.length === 0){
-            return true;
-        }
-        return checker( currentObject[toCheck[0]], toCheck.slice(1) );
+    if(noObjectsLeft){
+        return !!objectExists;
     }
-    return false;
+    return checker( currentObject[toCheck[0]], toCheck.slice(1) );
 }
 
-function checkObject(object){
+function checkObjectExists(object){
     if (typeof object === 'string'){
-        return checkObject(object.split('.'));
+        return checkObjectExists(object.split('.'));
     }
-
     const [head, ...tail] = object;
-    // debugger;
     return checker(window[head], tail);
-
-    // console.log( window[object[0]] );
-    // console.log( window[object[0]][object[1]] );
-    // console.log( window[object[0]][object[1]][object[2]] );
 }
 
 var foo = {bar: {baz: 'baz'}};
 
-checkObject('foo.bar.baz');
+checkObjectExists('foo.bar.baz');
