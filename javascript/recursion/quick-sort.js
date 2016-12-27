0e46e643fa3d1eabc74d99bdca6c5240c84d73ed
Richard Tan <richard.tan@crownbet.com.au>
2016-12-27 20:07:27 +1100

    Immutable quick sort

--- javascript/recursion/quick-sort.js
+++ javascript/recursion/quick-sort.js
@@ -1,7 +1,8 @@
 function swap(items, firstIndex, secondIndex){
-    var temp = items[firstIndex];
-    items[firstIndex] = items[secondIndex];
-    items[secondIndex] = temp;
+    let newItems = [...items];
+    newItems[firstIndex] = items[secondIndex];
+    newItems[secondIndex] = items[firstIndex];
+    return newItems;
 }
 
 function partition(items, leftIndex, rightIndex) {
@@ -19,29 +20,31 @@ function partition(items, leftIndex, rightIndex) {
         }
 
         if (leftIndex <= rightIndex) {
-            swap(items, leftIndex, rightIndex);
+            items = swap(items, leftIndex, rightIndex);
             leftIndex++;
             rightIndex--;
         }
     }
 
-    return leftIndex;
+    return {leftIndex, items};
 }
 
 function quickSort(items, leftIndex = 0, rightIndex = items.length - 1) {
-
+    // debugger;
     var index;
 
     if (items.length > 1) {
 
-        index = partition(items, leftIndex, rightIndex);
+        var partitionResult = partition(items, leftIndex, rightIndex);
+        index = partitionResult.leftIndex;
+        items = partitionResult.items;
 
         if (leftIndex < index - 1) {
-            quickSort(items, leftIndex, index - 1);
+            return quickSort(items, leftIndex, index - 1);
         }
 
         if (index < rightIndex) {
-            quickSort(items, index, rightIndex);
+            return quickSort(items, index, rightIndex);
         }
 
     }
