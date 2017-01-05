2a739c5473b7494913ec5435da56e6199362b283
Richard Tan <richard.tan@crownbet.com.au>
2017-01-06 00:56:46 +1100

    Created the best CURRY function ever

--- javascript/currying/curry.js
+++ javascript/currying/curry.js
@@ -2,16 +2,27 @@ function curry(fn){
     // look at the expected number of args
     const expectedNumArgs = fn.length;
     // compare it to the actual number of args
-    return function(...args){
+    function argumentCounter(...args){
         // if args length equal expected
         if(expectedNumArgs === args.length){
-            console.log('full');
+            return fn(...args);
+        }
+        else {
+            //return function partially filled
+            return function(...moreArgs){
+                return argumentCounter(...args, ...moreArgs)
+            }
         }
         console.log(args);
-        return fn(...args);
     };
+
+    return argumentCounter;
 }
 
 var add = curry(function(num1, num2){
     return num1 + num2;
+})
+
+var describePerson = curry(function(name, age, gender){
+    return `I'm ${name}, I'm ${age} years old, and I'm ${gender}`;
 })
