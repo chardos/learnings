8cf2d1a60e7d5821e9a36e48c7774a5a1f2c0086
Richard Tan <richard.tan@crownbet.com.au>
2017-01-29 13:34:44 +1100

    Conditional pipeline

---
+++ javascript/recursion/pipeline-conditional.js
@@ -0,0 +1,37 @@
+function add5(num){
+    return num + 5;
+}
+function multiply3(num){
+    return num * 3;
+}
+function halve(num){
+    return num / 2;
+}
+function dollarize(num){
+    return '$' + num;
+}
+
+function pipeline(...args){
+    return function(value){
+        return args.reduce((value, arg) => {
+            const isFunction = typeof arg === 'function';
+            const isArray = typeof arg === 'object';
+            const shouldRun = isFunction ||
+                              isArray && arg[1];
+            const fn = isFunction ? arg : arg[0];
+
+            if(shouldRun){ return fn(value) }
+            else { return value }
+
+
+        }, value);
+    }
+}
+
+const addAndMultiply = pipeline(
+    [add5, true],
+    multiply3,
+    halve,
+    dollarize
+)
+console.log(addAndMultiply(10));
