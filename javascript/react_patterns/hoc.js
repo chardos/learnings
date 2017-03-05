8f85bc00fcd7354ee8947718f0bc61977b72c165
Richard Tan <richard.tan@crownbet.com.au>
2017-03-06 10:11:04 +1100

    indenting fixed

--- javascript/react_patterns/hoc.js
+++ javascript/react_patterns/hoc.js
@@ -2,12 +2,26 @@
 // Adding props to a component
 
 function ppHOC(WrappedComponent) {
-  return class PP extends React.Component {
-    render() {
-      const newProps = {
-        user: currentLoggedInUser
-      }
-      return <WrappedComponent {...this.props} {...newProps}/>
+    return class PP extends React.Component {
+        render() {
+            const newProps = { user: currentLoggedInUser }
+            return <WrappedComponent { ...this.props } { ...newProps } />
+        }
     }
-  }
+}
+
+// Getting refs
+
+
+function refsHOC(WrappedComponent) {
+    return class RefsHOC extends React.Component {
+        proc(wrappedComponentInstance) {
+            wrappedComponentInstance.method()
+        }
+
+        render() {
+            const props = Object.assign({}, this.props, {ref: this.proc.bind(this)})
+            return <WrappedComponent {...props}/>
+        }
+    }
 }
