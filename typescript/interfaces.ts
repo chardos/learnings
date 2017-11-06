9141575e70a47d3a23c2b6aac350a0e272e5d02b
Richard Tan <richard.tan@crownbet.com.au>
2017-11-06 16:31:23 +1100

    Optional interfaces

---
+++ typescript/interfaces.ts
@@ -0,0 +1,36 @@
+interface LabelledValue {
+    label: string;
+}
+
+function printLabel(labelledObj: LabelledValue) {
+    console.log(labelledObj.label);
+}
+
+let myObj = {size: 10, label: "Size 10 Object"};
+printLabel(myObj);
+
+// ================================================
+// OPTIONAL PROPERTIES
+// ================================================
+
+// Question marks denote optional properties
+interface DogInterface {
+    legs?: number;
+    breed?: string;
+}
+
+interface CheckFace {
+    legs: number;
+    breed: string;
+    animal: string;
+}
+
+function createDog(dogConfig: DogInterface): CheckFace {
+    return {
+        legs: dogConfig.legs,
+        breed: dogConfig.breed,
+        animal: 'dog'
+    }
+}
+
+const dog = createDog({legs: 4})
